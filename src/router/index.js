458b9b46981e84ce8ed89791d501621b326247bc
fah <fah@ciklum.com>
2021-10-27 14:51:58 +0500

    Created crud and forms

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,30 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import AddProject from "../views/AddProject.vue";
+import EditProject from "../views/EditProject.vue";
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
+  { 
+    path: "/", 
+    name: "Home", 
+    component: Home 
+  },
+  { 
+    path: "/add-project", 
+    name: "AddProject", 
+    component: AddProject 
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    path: "/edit-project/:id",
+    name: "EditProject",
+    component: EditProject,
+    props: true,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,83 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="filter-nav">
+      <FilterNavs :current="current" @filterChange="handleFilters" />
+    </div>
+    <div v-if="projects.length">
+      <div v-for="project in filteredProjects" :key="project.id">
+        <SingleProject
+          :project="project"
+          @deleted="handleDelete"
+          @complete="handleComplete"
+        />
+      </div>
+    </div>
+    <div v-else>
+      Nothing to show here
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import SingleProject from "../components/SingleProject.vue";
+import FilterNavs from "../components/FilterNavs.vue";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: { SingleProject, FilterNavs },
+  data() {
+    return {
+      projects: [],
+      masterProjects: [],
+      current: "all",
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/projects")
+      .then((res) => res.json())
+      .then((data) => (this.projects = data , this.masterProjects = data))
+      .then((err) => console.log(err.message));
+  },
+  methods: {
+    handleDelete(projectid) {
+      this.projects = this.projects.filter((project) => {
+        return project.id !== projectid;
+      });
+    },
+    handleComplete(id) {
+      let p = this.projects.find((project) => {
+        return project.id === id;
+      });
+      p.complete = !p.complete;
+    },
+    handleFilters(currentClicked) {
+      this.current = currentClicked;
+      // if (currentClicked === "completed") {
+      //   this.projects = this.masterProjects.filter((project) => {
+      //     return project.complete === true;
+      //   });
+      // } else if (currentClicked === "ongoing"){
+      //   this.projects = this.masterProjects.filter((project) => {
+      //     return project.complete === false;
+      //   });
+      // }else{
+      //   this.projects =this.masterProjects;
+      // }
+    },
+  },
+  computed: {
+    filteredProjects() {
+      if (this.current === 'completed') {
+        return this.projects.filter((project) => {
+          return project.complete === true;
+        });
+      }
+      if (this.current === 'ongoing') {
+        return  this.projects.filter((project) => {
+          return project.complete === false;
+        });
+      }
+      return this.projects
+    }
+  },
+};
 </script>
